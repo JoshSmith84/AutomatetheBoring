46650a6491169f3e9913d1d7f60640acd9217444
JoshSmith84 <jas426@icloud.com>
2022-09-25 09:45:45 -0500

    Cleaned up with import.

--- collatz_sequence.py
+++ collatz_sequence.py
@@ -1,6 +1,9 @@
 # Automate the boring Stuff chapter 3 practice Project - Collatz Seq. - answer.
 # Author: Josh Smith
 
+from get_integer import get_integer
+
+
 # Answer to project
 def collatz(col_num: int) -> int:
     """Determines if input is even or odd, then runs appropriate
@@ -15,25 +18,6 @@ def collatz(col_num: int) -> int:
         col_num = 3 * col_num + 1
 
     return col_num
-
-
-# Pulled from another practice program I wrote to help test
-def get_integer(prompt):
-    """
-    Get an integer from Standard Input (stdin).
-    The function will continue looping, and prompting
-    the user,until a valid `int` is entered.
-
-    :param prompt: The String that the user will see
-         when they're prompted to enter the value.
-    :return: The integer that the user enters
-    """
-    while True:
-        temp = input(prompt)
-        if temp.isnumeric():
-            return int(temp)
-        else:
-            print("{} is not a valid number".format(temp))
 
 
 # Test block
