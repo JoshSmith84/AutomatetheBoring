2317e55faccf69982bffe06b3f17da21e23f3589
JoshSmith84 <jas426@icloud.com>
2022-10-26 10:47:04 -0500

    2048 strategy testing

--- 2048.py
+++ 2048.py
@@ -12,8 +12,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
 import time
-import openpyxl
+from openpyxl import Workbook, load_workbook
 from get_integer import get_integer
 
 
@@ -36,7 +37,7 @@ def book_strategy():
 
 def josh_strategy():
     global game
-    for i in range(5):
+    for i in range(10):
         game.send_keys(Keys.DOWN)
         wait()
         game.send_keys(Keys.RIGHT)
@@ -44,51 +45,83 @@ def josh_strategy():
     game.send_keys(Keys.UP)
     wait()
     game.send_keys(Keys.RIGHT)
+    wait()
 
 
-def run_game(strategy):
+def update_sheet(sheet_name):
+    global old_score
+    global wb
+    wb = load_workbook(wb_file)
+    sheet = wb[sheet_name]
+    new_row = sheet.max_row + 1
+    sheet.cell(row=new_row, column=1).value = old_score
+    wb.save(wb_file)
+
+
+def run_game(strategy, sample: int, sheet_name):
     global game
     global old_score
-    browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     browser.get('https://play2048.co/')
-
-    try:
-        game = browser.find_element(By.TAG_NAME, 'html')
-    except NoSuchElementException:
-        sys.exit("Unable to find the Game. Exiting program.")
-
-    bad_char = ['+', '\n']
-    old_score = 1
-    while True:
-        new_score = ''
+    p = browser.current_window_handle
+    time.sleep(1)
+    browser.switch_to.window(p)
+    for n in range(sample):
         try:
-            score = browser.find_element(
-                By.CLASS_NAME, 'score-container').text
-            strategy()
-            for char in score:
-                if char in bad_char:
-                    break
-                else:
-                    new_score += char
-            if int(new_score) != old_score:
-                old_score = int(new_score)
-                continue
-            else:
-                break
+            game = browser.find_element(By.TAG_NAME, 'html')
         except NoSuchElementException:
-            sys.exit("Trouble reading score container. Exiting program")
+            sys.exit("Unable to find the Game. Exiting program.")
 
+        bad_char = ['+', '\n']
+        old_score = 1
+        left_try = 0
+        while True:
+            #added this only for Josh_strategy since it occasionally
+            # got stuck and resulted in incorrect game over (I know, sloppy...)
+            new_score = ''
+            try:
+                score = browser.find_element(
+                    By.CLASS_NAME, 'score-container').text
+                strategy()
+                for char in score:
+                    if char in bad_char:
+                        break
+                    else:
+                        new_score += char
+                if int(new_score) != old_score:
+                    old_score = int(new_score)
+                    continue
+                else:
+                    if left_try == 0:
+                        game.send_keys(Keys.LEFT)
+                        left_try += 1
+                        continue
+                    else:
+                        update_sheet(sheet_name)
+                        try:
+                            restart = browser.find_element(
+                                By.CLASS_NAME, 'restart-button')
+                        except NoSuchElementException:
+                            sys.exit("Unable to find the restart button. "
+                                     "Exiting program.")
+                        restart.click()
+                        break
+            except NoSuchElementException:
+                sys.exit("Trouble reading score container. Exiting program")
     browser.quit()
-    return old_score
 
 
+folder = 'U:\\Joshua\\Dropbox\\Dropbox\\Python\\AutomatetheBoring\\'
 old_score = 1
 tries = get_integer('Input amount of tries:')
-for i in range(tries):
-    run_game(josh_strategy)
-    print(f'Game Over. Final score: {old_score}')
-    run_game(book_strategy)
-    print(f'Game Over. Final score: {old_score}')
+ad_block = f'{folder}Adblock-Plusfree-ad-blocker.crx'
+options = Options()
+options.add_extension(ad_block)
+wb = Workbook()
+wb_file = f'{folder}2048.xlsx'
+run_game(josh_strategy, tries, 'Josh')
+run_game(book_strategy, tries, 'Book')
+
 
 
 
